5788dc6cf79033debf420cdf2bc5c3f64f236172
omerfarukdeniz <omerfarukdeniz75@gmail.com>
2021-02-18 00:11:31 +0300

    18.02.2021

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -31,10 +31,10 @@ namespace WebAPI
             //Autofac, Ninject, CastleWindsor, StructureMap, LightInject, DryInject => IoC Container
             //AOP => 
             services.AddControllers();
-            services.AddSingleton<IProductService, ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
-            services.AddSingleton<ICategoryService, CategoryManager>();
-            services.AddSingleton<ICategoryDal, EfCategoryDal>();
+            //services.AddSingleton<IProductService, ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<ICategoryService, CategoryManager>();
+            //services.AddSingleton<ICategoryDal, EfCategoryDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
